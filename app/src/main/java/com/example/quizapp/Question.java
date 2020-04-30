6e22d5403f3ca7c12fd8148a0c194e3b31078959
Sokitbayev <Sokitbaev@gmail.com>
2020-05-01 02:59:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/quizapp/Question.java
@@ -0,0 +1,27 @@
+package com.example.quizapp;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+        this.mTextResId = mTextResId;
+    }
+}
